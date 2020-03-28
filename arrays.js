e41e6a73ca61acaeb0ca38053014f001ed95987c
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-28 14:58:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,13 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+
+  return array;
+}
